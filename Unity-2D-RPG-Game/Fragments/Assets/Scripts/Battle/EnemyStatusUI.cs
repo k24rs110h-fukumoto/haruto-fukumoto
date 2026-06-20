63d2eeab0057ed24f3c11abcb33cb9338b439058
Haruto Fukumoto <runoaima98@gmail.com>
2026-06-21 04:41:50 +0900

    Update Repository
    Rename folder
    Reflesh folder

---
+++ Unity-2D-RPG-Game/Fragments/Assets/Scripts/Battle/EnemyStatusUI.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStatusUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI enemyNameText;
+    [SerializeField] private TextMeshProUGUI enemyLevelText;
+    [SerializeField] private TextMeshProUGUI enemyHpText;
+    [SerializeField] private Image enemyImage;
+    [SerializeField] private Slider enemyHpSlider;
+
+    public void Setup(BattleEnemyData enemy)
+    {
+        enemyNameText.text = enemy.enemyData.enemyName;
+        enemyLevelText.text = "Lv. " + enemy.level.ToString();
+        enemyHpText.text = enemy.currentHp + " / " + enemy.maxHp;
+        enemyHpSlider.value = (float)enemy.currentHp / enemy.maxHp;
+        enemyImage.sprite = enemy.enemyData.battleImage;
+    }
+}
